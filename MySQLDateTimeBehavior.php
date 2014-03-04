1c14efa941acb880784c8dcd3eacb6c0ec9605a9
kikimor <i@kikimor.ru>
2014-03-05 00:47:51 +0600

    MySQLDateTimeBehavior for Yii

---
+++ MySQLDateTimeBehavior.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Class MySQLDateTimeBehavior
+ * Automatically converts date and datetime fields between ActiveRecord model and MySQL database.
+ *
+ * Author: kikimor <i@kikimor.ru>
+ * Version: 1.0
+ * Requires: Yii 1.0.9 version
+ */
+class MySQLDateTimeBehavior extends CActiveRecordBehavior
+{
+	/**
+	 * Date format (PHP date format)
+	 * @var string
+	 */
+	public $dateFormat = 'd.m.Y';
+	/**
+	 * DateTime format (PHP date format)
+	 * @var string
+	 */
+	public $dateTimeFormat = 'd.m.Y H:i';
+
+	/**
+	 * @param CEvent $event
+	 */
+	public function afterFind($event)
+    {
+        foreach ($event->sender->tableSchema->columns as $columnName => $column){
+            if ($column->dbType != 'date' and $column->dbType != 'datetime') continue;
+
+            if (($timestamp = strtotime($event->sender->$columnName)) !== false) {
+				switch ($column->dbType) {
+					case 'date':
+						$event->sender->$columnName = date($this->dateFormat, $timestamp);
+						break;
+					case 'datetime':
+						$event->sender->$columnName = date($this->dateTimeFormat, $timestamp);
+						break;
+				}
+			}
+        }
+    }
+
+	/**
+	 * @param CModelEvent $event
+	 */
+	public function beforeSave($event)
+    {
+        foreach ($event->sender->tableSchema->columns as $columnName => $column){
+            if ($column->dbType != 'date' and $column->dbType != 'datetime') continue;
+
+			if (($timestamp = strtotime($event->sender->$columnName)) !== false) {
+				$event->sender->$columnName = new CDbExpression('STR_TO_DATE(:date, "%d%m%Y%H%i%s")', [':date' => date('dmYHis', $timestamp)]);
+			}
+		}
+    }
+}
